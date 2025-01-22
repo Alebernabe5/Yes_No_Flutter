9a3629c1cbb79cf39d48c10c5655c2c8c080b5f2
Alejandro <alebernadev@gmail.com>
2025-01-22 12:43:13 +0100

    Añado theme al proyecto

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color.fromARGB(15, 31, 13, 192);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Color.fromARGB(255, 247, 4, 4),
+  Color.fromARGB(255, 118, 255, 64),
+  Color.fromARGB(255, 255, 109, 64),
+  Color.fromARGB(255, 64, 255, 137),
+  Color.fromARGB(255, 201, 15, 92),
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+            'colors must be between and ${_colorThemes.length}');
+
+  ThemeData theme() {
+    return ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed:
+            _colorThemes[selectedColor], //Hacer paleta de colores en theme
+        brightness: Brightness.dark); //Para ponerlo en modo oscuro
+  }
+}
